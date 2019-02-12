283f7cb218d98b13556dfe52ef6f992312525658
Duy Vu <39290682+Rosebud303@users.noreply.github.com>
2019-02-12 16:00:52 -0700

    Update smoothies.js

--- smoothies.js
+++ smoothies.js
@@ -2,15 +2,15 @@ const smoothies = [{
   "name": "Babe Ruth Strawberry Pineapple Banana Smoothie",
   "ingredients": [
     "1 cup strawberries",
-    "1/2 cup pineapple",
+    "½ cup pineapple",
     "1 banana",
     "2 cups orange juice",
-    "1/2 cup greek yogurt",
+    "½ cup greek yogurt",
     "1 cup spinach, optional",
     "1 tablespoon chia or flaxseed, optional",
     "ice"
   ],
-  "servings": "4",
+  "servings": 4,
   "source": "https://www.modernhoney.com/6-healthy-superfood-smoothies/",
   "img": "images/babe-ruth.png"
 },
@@ -22,10 +22,10 @@ const smoothies = [{
     "3 tablespoons raw almonds",
     "2 tablespoons raw honey",
     "1 teaspoon cinnamon",
-    "1/2 teaspoon almond extract",
+    "½ teaspoon almond extract",
     "1 tablespoon coconut oil"
   ],
-  "servings": "1",
+  "servings": 1,
   "source": "https://bakedbree.com/cherry-almond-smoothies",
   "img": "images/cherry-almond.png"
 },
@@ -34,27 +34,27 @@ const smoothies = [{
   "ingredients": [
     "1 cup pineapple",
     "2 cups spinach",
-    "1/2 cup grapes",
-    "1 1/2 cup orange juice",
+    "½ cup grapes",
+    "1 ½ cup orange juice",
     "1 banana",
     "ice"
   ],
-  "servings": "4",
+  "servings": 4,
   "source": "https://www.modernhoney.com/6-healthy-superfood-smoothies/",
   "img": "images/lovely-greens.png"
 },
 {
   "name": "Blueberry Cacao Power Smoothie",
   "ingredients": [
-    "3/4 cup frozen blueberries",
-    "3/4 cup almond milk",
+    "¾ cup frozen blueberries",
+    "¾ cup almond milk",
     "2 tablespoons raw cacao powder",
     "1 tablespoon chia seeds",
     "2-3 pitted dates",
     "1 cup power greens mix",
     "stevia or pure maple syrup, to taste (optional)"
   ],
-  "servings": "",
+  "servings": 2,
   "source": "https://www.hummusapien.com/blueberry-cocao-power-smoothie/",
   "img": "images/blueberry-cacao-power.png"
 },
@@ -64,13 +64,13 @@ const smoothies = [{
     "2 cups kale or power greens mix",
     "2 cups baby spinach",
     "2 cups apple juice",
-    "1/2 cucumber",
-    "1/2 lemon squeezed",
+    "½ cucumber",
+    "½ lemon squeezed",
     "1 banana",
     "ice",
     "1 teaspoon fresh ginger grated, optional"
   ],
-  "servings": "4",
+  "servings": 4,
   "source": "https://www.modernhoney.com/youthful-glow-green-smoothie/",
   "img": "images/youthful-glow-green.png"
 },
@@ -78,13 +78,13 @@ const smoothies = [{
   "name": "Peach Mango Smoothie",
   "ingredients": [
     "1 cup peaches",
-    "1/2 cup mangoes",
-    "1/4 cup mango nectar",
-    "1/2 a banana",
+    "½ cup mangoes",
+    "¼ cup mango nectar",
+    "½ a banana",
     "2 teaspoons sugar or honey (optional)",
     "2 ice cubes"
   ],
-  "servings": "1",
+  "servings": 1,
   "source": "https://www.onceuponachef.com/recipes/peach-mango-smoothies.html",
   "img": "images/peach-mango.png"
 },
@@ -99,7 +99,7 @@ const smoothies = [{
     "¼ cup plain yogurt",
     "1 tablespoon ground flaxseeds"
   ],
-  "servings": "2",
+  "servings": 2,
   "source": "https://www.bonappetit.com/recipe/mango-berry-banana-smoothie",
   "img": "images/mango-berry-banana.png"
 },
@@ -109,9 +109,9 @@ const smoothies = [{
     "1 frozen banana, broken into 3–4 chunks",
     "1 cup almond milk",
     "1 tablespoon almond butter",
-    "1/8 teaspoon almond extract (optional)]"
+    "½ teaspoon almond extract (optional)]"
   ],
-  "servings": "1",
+  "servings": 1,
   "source": "https://www.bonappetit.com/recipe/banana-almond-smoothie",
   "img": "images/banana-almond.png"
 },
@@ -122,11 +122,11 @@ const smoothies = [{
     "2 cups (packed) spinach leaves",
     "1 cup frozen blackberries",
     "1 cup nonfat yogurt",
-    "1/2 cup fresh orange juice",
+    "½ cup fresh orange juice",
     "1 teaspoon finely grated peeled ginger",
     "1 teaspoon honey or light agave syrup (nectar)"
   ],
-  "servings": "1",
+  "servings": 1,
   "source": "https://www.bonappetit.com/recipe/blackberry-and-yogurt-breakfast-smoothie",
   "img": "images/backberry-and-yogurt-breakfast.png"
 },
@@ -143,7 +143,7 @@ const smoothies = [{
     "1 tablespoon light agave nectar",
     "Pinch of kosher salt"
   ],
-  "servings": "1",
+  "servings": 1,
   "source": "https://www.bonappetit.com/recipe/avocado-kale-pineapple-and-coconut-smoothie",
   "img": "images/avocado-kale-pineapple-and-coconut.png"
 },
@@ -160,7 +160,7 @@ const smoothies = [{
     "Pinch of cayenne pepper",
     "Pinch of kosher salt"
   ],
-  "servings": "1",
+  "servings": 1,
   "source": "https://www.bonappetit.com/recipe/tropical-carrot-ginger-and-turmeric-smoothie",
   "img": "images/tropical-carrot-ginger-and-turmeric.png"
 },
@@ -169,10 +169,10 @@ const smoothies = [{
   "ingredients": [
     "2 cups fresh strawberries, halved",
     "1 banana, quartered and frozen",
-    "1/2 cup Greek yogurt",
-    "1/2 cup milk"
+    "½ cup Greek yogurt",
+    "½ cup milk"
   ],
-  "servings": "2",
+  "servings": 2,
   "source": "https://downshiftology.com/recipes/strawberry-banana-smoothie",
   "img": "images/strawberry-banana.jpg"
 }, 
@@ -180,12 +180,12 @@ const smoothies = [{
   "name": "Blackberry Coconut Fruit Smoothie",
   "ingredients": [
     "1 cup unsweetened coconut milk",
-    "1/2 cup blackberries, fesh or frozen",
-    "1/2 banana",
-    "1/4 cup unsweetened coconut flakes",
+    "½ cup blackberries, fesh or frozen",
+    "½ banana",
+    "½ cup unsweetened coconut flakes",
     "2 scoops of Vanilla Protein Powder"
   ],
-  "servings": "1",
+  "servings": 1,
   "source": "https://www.asweetpeachef.com/4-high-protein-fruit-smoothie-recipes",
   "img": "images/blackberry-coconut-fruit.png"
 },
@@ -197,7 +197,7 @@ const smoothies = [{
     "½ cup pineapple chunks, fresh or frozen",
     "2 scoops of Vanilla Protein Powder"
   ],
-  "servings": "1",
+  "servings": 1,
   "source": "https://www.asweetpeachef.com/4-high-protein-fruit-smoothie-recipes",
   "img": "images/tropical-chai-fruit.png"
 },
@@ -206,25 +206,25 @@ const smoothies = [{
   "ingredients": [
     "1 cup unsweetened almond milk",
     "1 cup baby spinach, packed",
-    "1/2 banana",
-    "1/2 pear",
+    "½ banana",
+    "½ pear",
     "1 tbsp almond butter",
     "2 scoops of Vanilla Protein Powder"
   ],
-  "servings": "1",
+  "servings": 1,
   "source": "https://www.asweetpeachef.com/4-high-protein-fruit-smoothie-recipes",
   "img": "images/the-best-green-protein-fruit.png"
 }, 
 {
   "name": "Tropical Smoothie Recipe",
   "ingredients": [
-    "3/4 cup of pineapple juice",
-    "1/2 cup of canned coconut milk light is ok",
+    "½ cup of pineapple juice",
+    "½ cup of canned coconut milk light is ok",
     "1 large banana fresh or sliced then frozen",
     "1 cup of frozen mango",
     "1 cup of frozen pineapple"
   ],
-  "servings": "2",
+  "servings": 2,
   "source": "https://www.dinneratthezoo.com/tropical-smoothie-recipe/",
   "img": "images/tropical-smoothie.png"
 },
@@ -238,20 +238,20 @@ const smoothies = [{
     "1 tablespoon peanut butter, creamy",
     "1 cup almond milk"
   ],
-  "servings": "2",
+  "servings": 2,
   "source": "https://fitfoodiefinds.com/the-best-breakfast-smoothie/",
   "img": "images/the-best-breakfast-smoothie.png"
 },
 {
   "name": "Aloha Pineapple Smoothie",
   "ingredients": [
-    "1/2 banana, sliced",
-    "1/2 cup strawberries, sliced",
-    "1/2 cup fresh pineapple, diced",
+    "½ banana, sliced",
+    "½ cup strawberries, sliced",
+    "½ cup fresh pineapple, diced",
     "1 cup crushed ice",
-    "1/2 cup vanilla greek yogurt"
+    "½ cup vanilla greek yogurt"
   ],
-  "servings": "1",
+  "servings": 1,
   "source": "https://www.sixsistersstuff.com/recipe/copycat-jamba-juice-aloha-pineapple-smoothie-recipe/",
   "img": "images/aloha-pineapple.png"
 },
@@ -259,12 +259,12 @@ const smoothies = [{
   "name": "Immunity Boosting Triple Berry Kiwi Smoothie",
   "ingredients": [
     "1 cup frozen strawberries",
-    "3/4 cup frozen raspberries",
-    "1/2 cup frozen blueberries",
+    "½ cup frozen raspberries",
+    "½ cup frozen blueberries",
     "2 kiwifruit peeled and sliced",
     "1 cup orange juice"
   ],
-  "servings": "1",
+  "servings": 1,
   "source": "https://theprettybee.com/triple-berry-kiwi-smoothie/?sf=pywonlz",
   "img": "images/immunity-boosting-triple-berry-kiwi.png"
 },
@@ -285,9 +285,9 @@ const smoothies = [{
   "name": "Orange Creamsicle Smoothie",
   "ingredients": [
     "1 cup your favorite type of milk",
-    "3/4 cup chopped baby carrots",
+    "¾ cup chopped baby carrots",
     "5 clementine mandarin oranges peeled and separated",
-    "1/2 cup unsweetened yogurt",
+    "½ cup unsweetened yogurt",
     "Optional: lemon juice Stevia drops"
   ],
   "servings": 2,
@@ -299,10 +299,10 @@ const smoothies = [{
   "ingredients": [
     "1/8 cup creamy peanut butter",
     "1 banana (frozen is best if you have it)",
-    "1/4 cup milk (add another 1/4 cup if you have a lower powered blender)",
-    "1/4 cup low fat vanilla greek yogurt",
+    "¼ cup milk (add another 1/4 cup if you have a lower powered blender)",
+    "¼ cup low fat vanilla greek yogurt",
     "1 TBS cocoa powder",
-    "1/2 cup ice"
+    "½ cup ice"
   ],
   "servings": 2,
   "source": "https://lmld.org/chocolate-peanut-butter-banana-smoothie/",
@@ -312,7 +312,7 @@ const smoothies = [{
   "name": "Glowing Green Spirulina Smoothie",
   "ingredients": [
     "1 banana, frozen",
-    "1/4 cup blueberries, frozen",
+    "¼ cup blueberries, frozen",
     "1 cup kale",
     "1 cup unsweetened soy milk (or milk of choice)",
     "1 teaspoon spirulina powder"
@@ -326,7 +326,7 @@ const smoothies = [{
   "ingredients": [
     "1 cup mango, frozen",
     "1 cup sweet cherries, frozen",
-    "1 1/4 cup water"
+    "1 ¼ cup water"
   ],
   "servings": 2,
   "source": "https://www.pinterest.com/pin/359513982735684622/",
@@ -351,11 +351,11 @@ const smoothies = [{
 {
   "name": "Copycat Jamba Juice Aloha Pineapple",
   "ingredients": [
-    "1/2 banana",
-    "1/2 cup yogurt",
-    "1/2 cup pineapple, fresh",
+    "½ banana",
+    "½ cup yogurt",
+    "½ cup pineapple, fresh",
     "1 cup ice cubes",
-    "1/2 strawberries"
+    "½ strawberries"
   ],
   "servings": 2,
   "source": "https://www.sixsistersstuff.com/recipe/copycat-jamba-juice-aloha-pineapple-smoothie-recipe/",
@@ -378,13 +378,13 @@ const smoothies = [{
 {
   "name": "Blueberry Muffin Smoothie",
   "ingredients": [
-    "1/2 cup milk",
+    "½ cup milk",
     "4 – 6 oz vanilla Greek yogurt",
-    "1/2 cup frozen blueberries",
-    "1/2 frozen banana",
-    "1/4 cup raw oats",
-    "1/4 teaspoon lemon zest",
-    "1/2 cup ice cubes"
+    "½ cup frozen blueberries",
+    "½ frozen banana",
+    "¼ cup raw oats",
+    "¼ teaspoon lemon zest",
+    "½ cup ice cubes"
   ],
   "servings": 1,
   "source": "https://iowagirleats.com/2014/07/09/blueberry-muffin-smoothie/",
@@ -395,7 +395,7 @@ const smoothies = [{
   "ingredients": [
     "3 cups chopped watermelon",
     "1 ripe peach sliced",
-    "1/2 cup plain coconut milk refrigerated, not canned",
+    "½ cup plain coconut milk refrigerated, not canned",
     "handful of ice",
     "mint for garnish"
   ],
@@ -407,10 +407,10 @@ const smoothies = [{
   "name": "Cinnamon Roll Smoothie",
   "ingredients": [
     "1 cup vanilla almond milk",
-    "1/2 cup vanilla Greek yogurt",
-    "1/4 cup old fashioned oats",
+    "½ cup vanilla Greek yogurt",
+    "¼ cup old fashioned oats",
     "1 Tablespoon brown sugar",
-    "1/4 teaspoon cinnamon",
+    "¼ teaspoon cinnamon",
     "1 frozen banana (or fresh banana, but add in 3-4 ice cubes with fresh)"
   ],
   "servings": 2,
@@ -420,7 +420,7 @@ const smoothies = [{
 {
   "name": "Cookies and Cream Smoothie",
   "ingredients": [
-    "1/2 frozen banana",
+    "½ frozen banana",
     "1 cup vanilla yogurt or plain yogurt",
     "1 tsp vanilla extract",
     "1 cup milk of choice",
@@ -434,7 +434,7 @@ const smoothies = [{
 {
   "name": "Anti-Bloat Smoothie",
   "ingredients": [
-    "1/2 cup coconut water",
+    "½ cup coconut water",
     "1 banana",
     "1 large cucumber, sliced",
     "1 inch piece of fresh ginger, peeled and sliced",
@@ -449,10 +449,10 @@ const smoothies = [{
   "ingredients": [
     "6 oz vanilla yogurt",
     "1 apple chopped",
-    "1/2 cup milk",
+    "½ cup milk",
     "1 Tablespoon of  honey",
-    "1/2 cup ice cubes",
-    "1/4 teaspoon cinnamon"
+    "½ cup ice cubes",
+    "¼ teaspoon cinnamon"
   ],
   "servings": 2,
   "source": "https://www.familyfreshmeals.com/2015/08/healthy-apple-pie-smoothie.html",
@@ -461,10 +461,10 @@ const smoothies = [{
 {
   "name": "Healthy Pina Colada Smoothie",
   "ingredients": [
-    "1/2 frozen banana",
-    "1/2 cup frozen pineapple chunks",
+    "½ frozen banana",
+    "½ cup frozen pineapple chunks",
     "1/3 cup pineapple or pina colada juice",
-    "1/4 cup coconut milk",
+    "¼ cup coconut milk",
     "a handful of ice cubes"
   ],
   "servings": 1,
@@ -474,11 +474,11 @@ const smoothies = [{
 {
   "name": "Raspberry Apple Smoothie with Bananas",
   "ingredients": [
-    "1/2 cup coconut water",
+    "½ cup coconut water",
     "2 medium-sized bananas",
     "2 cups apple, sliced, (1 large apple)",
     "2 cups raspberries, frozen",
-    "1/2 cup ice cubes"
+    "½ cup ice cubes"
   ],
   "servings": 2,
   "source": "https://www.jessicagavin.com/raspberry-apple-smoothie-bananas/",
@@ -487,10 +487,10 @@ const smoothies = [{
 {
   "name": "Cherry Smoothie",
   "ingredients": [
-    "1 1/2 cups juice cherry juice, cran-cherry juice, apple juice or white grape juice",
-    " 1 banana cut in half",
-    "1 1/2 cups frozen dark sweet cherries",
-    "3/4 cup vanilla Greek yogurt",
+    "1 ½ cups juice cherry juice, cran-cherry juice, apple juice or white grape juice",
+    "1 banana cut in half",
+    "1 ½ cups frozen dark sweet cherries",
+    "¾ cup vanilla Greek yogurt",
     "fresh cherries and mint sprigs for garnish optional"
   ],
   "servings": 2,
@@ -513,9 +513,9 @@ const smoothies = [{
   "name": "Mango Vanilla Smoothie",
   "ingredients": [
     "1 cup frozen mango chunks",
-    "1/4 cup 2 ounces plain Greek yogurt",
-    "1/4 cup vanilla soymilk",
-    "1/2 teaspoon vanilla extract"
+    "¼ cup 2 ounces plain Greek yogurt",
+    "¼ cup vanilla soymilk",
+    "½ teaspoon vanilla extract"
   ],
   "servings": 1,
   "source": "https://gatherforbread.com/mango-vanilla-smoothie/",
@@ -528,8 +528,8 @@ const smoothies = [{
     "1 tbsp almond butter",
     "1 frozen banana",
     "2 tbsp vanilla protein powder",
-    "1/4 cup oats",
-    "1/2 tsp cinnamon",
+    "¼ cup oats",
+    "½ tsp cinnamon",
     "Pinch of pink sea salt"
   ],
   "servings": 1,
